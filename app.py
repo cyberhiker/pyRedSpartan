9cc0ad4f1c24a3249950ffa961999f739807caac
Chris Burton <cburton@redeyetek.com>
2016-01-27 10:42:42 -0500

    Beginning to switch over to PostgreSQL

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from flask.ext.bcrypt import Bcrypt
 from flask.ext.csrf import csrf
+from flask.ext.sqlalchemy import SQLAlchemy
 import logging
+import os
 
 app = Flask(__name__)
 app.config.from_object('settings')
 bcrypt = Bcrypt(app)
 csrf(app)
+db = SQLAlchemy(app)
 
 from models import *
 
 @app.route('/')
 def index():
-    # posts = Post.objects.all()
     roles = AssessmentRoles.objects.all()
     return render_template('index.html', roles=roles)
 
--- settings.py
+++ settings.py
@@ -1,3 +1,38 @@
-DEBUG = True
+import os
 
-SECRET_KEY = 'sdfsdf82347$$%$%$%$&fsdfs!!ASx+__WEBB$'
+class Config(object):
+    DEBUG = True
+    TESTING = True
+    TRAP_HTTP_EXCEPTIONS = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_ENABLED = True
+    SECRET_KEY = 'sdfsdf82347$$%$%$%$&fsdfs!!ASx+__WEBB$'
+    DATABASE = {
+    'drivername': 'postgres',
+    'host': 'localhost',
+    'port': '5432',
+    'username': 'postgres',
+    'password': 'postgres',
+    'database': 'redspartan'
+    }
+    SQLALCHEMY_DATABASE_URI = 'postgres://postgres:postgres@localhost:5432/redspartan'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+'''
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+'''
